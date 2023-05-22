89639ccb305e437fc52f1e4b6533c5b6ae67b88c
H Uyen Mlo <uyenuyenbt4869@gmail.com>
2023-05-23 01:42:04 +0700

    update router three

--- back-end/app/router/controller.py
+++ back-end/app/router/controller.py
@@ -1,7 +1,12 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template,send_from_directory
 router = Blueprint("router", __name__)
 
 @router.route('/', defaults={'path': ''})
 @router.route('/<path:path>')
-def catch_all(path):
-    return render_template('index.html')
+def serve(path):
+    if path != "" and not path.startswith("api") and not path.startswith("static"):
+        # Chuyển hướng các yêu cầu không phù hợp đến tệp "index.html" của React
+        return send_from_directory('../front-end/build', 'index.html')
+    else:
+        # Gửi các yêu cầu khác đến Flask để xử lý
+        return send_from_directory('./', path)
