4ae7ab2b5b274bdc6c983498a4c37e6d48a07e9b
H Uyen Mlo <uyenuyenbt4869@gmail.com>
2023-05-23 18:19:23 +0700

    fig router two

--- back-end/app/router/controller.py
+++ back-end/app/router/controller.py
@@ -1,24 +1,19 @@
 from flask import Blueprint, render_template,send_from_directory
 router = Blueprint("router", __name__)
-
-# @router.route('/', defaults={'path': ''})
-# @router.route('/<path:path>')
-# def serve(path):
-#     if path != "" and not path.startswith("api") and not path.startswith("static"):
-#         # Chuyển hướng các yêu cầu không phù hợp đến tệp "index.html" của React
-#         return send_from_directory('../front-end/build', 'index.html')
-#     else:
-#         # Gửi các yêu cầu khác đến Flask để xử lý
-#         return send_from_directory('./', path)
-
-# @router.errorhandler(404)
-# def not_found(e):
-#     return send_from_directory('../front-end/build', 'index.html')
-
-@router.route('/')
-def index():
-    return render_template('index.html')
-
+import os
+@router.route('/', defaults={'path': ''})
 @router.route('/<path:path>')
-def catch_all(path):
-    return render_template('index.html')
+def serve(path):
+    if path != "" and not path.startswith("api") and not path.startswith("static"):
+        return send_from_directory('static', 'index.html')
+    else:
+        return send_from_directory('./', path)
+
+@router.errorhandler(404)
+def not_found(e):
+    return send_from_directory('static', 'index.html')
+
+# @router.route('/<path:filename>')
+# def serve_static():
+#     root_dir = os.path.dirname(os.path.abspath(__file__))
+#     return send_from_directory(os.path.join(root_dir, 'static'), 'index.html')
