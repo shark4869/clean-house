ad47da5b724c4f94bab08b09c63b76e0a0953be3
H Uyen Mlo <uyenuyenbt4869@gmail.com>
2023-05-23 20:07:42 +0700

    fig router reload

--- back-end/app/router/controller.py
+++ back-end/app/router/controller.py
@@ -1,18 +1,17 @@
 from flask import Blueprint, render_template,send_from_directory
 router = Blueprint("router", __name__)
 import os
-@router.route('/', defaults={'path': ''})
-@router.route('/<path:path>')
-def serve(path):
-    if path != "" and not path.startswith("api") and not path.startswith("static"):
-        root_dir = os.path.dirname(os.path.abspath(__file__))
-        return send_from_directory(os.path.join(root_dir, 'static'), 'index.html')
-    else:
-        return send_from_directory('./', path)
+# @router.route('/', defaults={'path': ''})
+# @router.route('/<path:path>')
+# def serve(path):
+#     if path != "" and not path.startswith("api") and not path.startswith("static"):
+#         root_dir = os.path.dirname(os.path.abspath(__file__))
+#         return send_from_directory(os.path.join(root_dir, 'static'), 'index.html')
+#     else:
+#         return send_from_directory('./', path)
 
+@router.route('/static/<path:filename>')
+def serve_static(filename):
+    root_dir = os.path.dirname(os.path.abspath(__file__))
+    return send_from_directory(os.path.join(root_dir, 'static'), filename)
 
-
-# @router.route('/<path:filename>')
-# def serve_static():
-#     root_dir = os.path.dirname(os.path.abspath(__file__))
-#     return send_from_directory(os.path.join(root_dir, 'static'), 'index.html')
